d3ad51d48ebff30746dfeb331496f9be3d186fe0
sunainaojha <sunainaojha1242@gmail.com>
2023-11-04 14:55:21 -0400

    craeted the folder structures and added the deoendencies

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+// Wrap Mongoose around local connection to MongoDB
+mongoose.connect('mongodb://localhost:27017/userThoughtDB', {
+  // useFindAndModify: false,  
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+// Export connection 
+module.exports = mongoose.connection;
